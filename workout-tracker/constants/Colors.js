ce0bdce722a48ca559533a192f2bd240ecbc16df
Devansh <112242677+devanshkp@users.noreply.github.com>
2025-08-10 20:06:56 +1000

    Create Colors.js

---
+++ workout-tracker/constants/Colors.js
@@ -0,0 +1,27 @@
+export const Colors = {
+  warning: "hsl(7, 75.44%, 55.29%)",
+  warmup: "hsl(27, 76.64%, 58.04%)",
+  accent: "hsl(200, 100%, 50%)",
+
+  dark: {
+    bgPrimary: "hsl(0, 0%, 0%)",
+    bgSecondary: "hsl(0, 0%, 12.16%)",
+    bgTertiary: "hsl(0, 0%, 21.18%)",
+    bgNavbar: "hsla(0, 0%, 12.16%, 0.85)",
+    bgTextField: "hsl(0, 0%, 7.06%)",
+    textPrimary: "hsl(0, 0%, 94.51%)",
+    textSubtle: "hsl(0, 0%, 60%)",
+    border: "hsla(0, 0%, 100%, 0.1)",
+  },
+
+  light: {
+    bgPrimary: "hsl(0, 0%, 0%)",
+    bgSecondary: "hsl(0, 0%, 12.16%)",
+    bgTertiary: "hsl(0, 0%, 21.18%)",
+    bgNavbar: "hsla(0, 0%, 12.16%, 0.85)",
+    bgTextField: "hsl(0, 0%, 7.06%)",
+    textPrimary: "hsl(0, 0%, 94.51%)",
+    textSubtle: "hsl(0, 0%, 60%)",
+    border: "hsla(0, 0%, 100%, 0.1)",
+  },
+};
